fa78766d9dd783de1ba6a2a2679555c6c541a378
unhaten <csgold2002@mail.ru>
2024-03-11 00:16:45 +0300

    theme config

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google'
 import './globals.css'
 import { Inter as FontSans } from 'next/font/google'
 import { cn } from '@/lib/utils'
+import { ThemeProvider } from './components/theme-provider'
 
 // const inter = Inter({ subsets: ['latin'] })
 
@@ -30,7 +31,14 @@ export default function RootLayout({
 				)}
 				// className={inter.className}
 			>
-				{children}
+				<ThemeProvider
+					attribute='class'
+					defaultTheme='system'
+					enableSystem
+					disableTransitionOnChange
+				>
+					{children}
+				</ThemeProvider>
 			</body>
 		</html>
 	)
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,32 +1,40 @@
 import { FC } from 'react'
+import { Button } from './components/ui/button'
+import { ModeToggle } from './components/ui/toggle-mode'
 
 interface HomeProps {}
 
 const Home: FC<HomeProps> = async () => {
-	const data = await fetch('https://helldivers-2.fly.dev/api/').then(res =>
-		res.json()
-	)
+	// const data = await fetch('https://helldivers-2.fly.dev/api/').then(res =>
+	// 	res.json()
+	// )
 
-	const currentWarId = data.current
+	// const currentWarId = data.current
 
-	console.log(currentWarId)
+	// console.log(currentWarId)
 
-	const war = await fetch(
-		`https://helldivers-2.fly.dev/api/${currentWarId}/planets`
-	).then(res => res.json())
+	// const war = await fetch(
+	// 	`https://helldivers-2.fly.dev/api/${currentWarId}/planets`
+	// ).then(res => res.json())
 
-	console.log(war)
+	// console.log(war)
 
-	const planet = await fetch(
-		`https://helldivers-2.fly.dev/api/${currentWarId}/planets/64` // meridia
-		// 125- fenrir III
-	).then(res => res.json())
+	// const planet = await fetch(
+	// 	`https://helldivers-2.fly.dev/api/${currentWarId}/planets/64` // meridia
+	// 	// 125- fenrir III
+	// ).then(res => res.json())
 
-	console.log(planet)
+	// console.log(planet)
 
 	return (
 		<main>
-			<p>this is a test</p>
+			<div className='bg-background'>
+				<h1 className='text-3xl font-bold underline bg-teal-600'>
+					Helldive Haven
+				</h1>
+				<Button>click me</Button>
+				<ModeToggle />
+			</div>
 		</main>
 	)
 }
